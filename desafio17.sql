b1545b676c92fb5b71117d42dec161a9b6c2f60a
Mariana Pereira <marianapereira.s@live.com>
2023-04-21 16:06:42 -0300

    feat: Requisito 18

--- desafio17.sql
+++ desafio17.sql
@@ -1,5 +1,5 @@
 SELECT id, supplier_id FROM purchase_orders
   WHERE supplier_id = 1
-  OR supplier_id = 3
-  OR supplier_id = 5
-  OR supplier_id = 7;
+    OR supplier_id = 3
+    OR supplier_id = 5
+    OR supplier_id = 7;
