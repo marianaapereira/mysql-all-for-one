591d741f4f2b59cbfb9231c896ff5c6fb534a07b
Mariana Pereira <marianapereira.s@live.com>
2023-04-21 15:46:49 -0300

    feat: Requisito 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,2 @@
 SELECT notes FROM purchase_orders
   WHERE notes LIKE 'Purchase generated based on Order #3_';
-
--- 11 - Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de Purchase generated based on Order é maior ou igual a 30 e menor ou igual a 39.
-
--- ✨ Dica: Purchase generated based on Order é um valor atribuído à coluna notes e não uma coluna.
